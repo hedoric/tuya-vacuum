9aede11ea34ee9051a7b96572d14a9f89fad529f
Jaiden <38144027+jaidenlab@users.noreply.github.com>
2024-10-28 21:15:00 -0400

    Migrated code from tuya-vacuum-maps

---
+++ tests/test_tuya.py
@@ -0,0 +1,30 @@
+"""Functional test for communication with the Tuya Cloud API."""
+
+import logging
+import os
+
+from dotenv import load_dotenv
+
+from tuya_vacuum.tuya import TuyaCloudAPI
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def test_tuya_request():
+    """Functionally test the tuya_request."""
+
+    # Load environment variables
+    load_dotenv()
+
+    # Get environment variables
+    CLIENT_ID = os.environ["CLIENT_ID"]
+    CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+    DEVICE_ID = os.environ["DEVICE_ID"]
+
+    BASE = "https://openapi.tuyaus.com"
+    ENDPOINT = f"/v1.0/users/sweepers/file/{DEVICE_ID}/realtime-map"
+    tuya = TuyaCloudAPI(base=BASE, client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+
+    response = tuya.request(ENDPOINT)
+    print(response)
+    assert response["success"]
